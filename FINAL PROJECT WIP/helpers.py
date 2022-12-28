5025380fd1c7785ccaa7599823a6c6521190dbbd
Larry <102634983+clazzy0@users.noreply.github.com>
2022-12-27 19:51:02 -0500

    Delete FINAL PROJECT WIP directory

--- FINAL PROJECT WIP/helpers.py
+++
@@ -1,6 +0,0 @@
-import requests
-import os
-from flask import Flask, render_template
-
-def error(message, code=400):
-    return render_template("error.html", top=code, bottom=message.upper()), code
